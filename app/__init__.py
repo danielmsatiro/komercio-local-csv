3c398c16dba8e6c7cc99f6244967812fd6aa3c5b
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-02-23 09:40:18 -0300

    feat: add routes post, patch and delete

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
+from crypt import methods
+from turtle import update
 from flask import Flask, request
-from app.products import get_products_csv
+from app.products import get_products_csv, create_product_csv, update_product_csv, delete_product_csv
 
 app = Flask(__name__)
 
 @app.get('/products')
-@app.get('/products/<product_id>')
+@app.get('/products/<int:product_id>')
 def products(product_id=None):
     page = request.args.get("page")
     per_page = request.args.get("per_page")
@@ -14,11 +16,18 @@ def products(product_id=None):
         per_page = int(per_page)
     finally:
         return get_products_csv(product_id, page or 1, per_page or 3)
-    
+
+@app.post('/products')
+def create_product():
+    data = request.get_json()
+    return create_product_csv(data)
+
+@app.patch('/products/<int:product_id>')
+def update_product(product_id):
+    data = request.get_json()
+    return update_product_csv(data, product_id)
 
 
-""" @app.post('/products')
-
-@app.patch('/products/<product_id>')
-
-@app.delete('/products/<product_id>') """
+@app.delete('/products/<int:product_id>')
+def delete_product(product_id):
+    return delete_product_csv(product_id)
--- app/products/__init__.py
+++ app/products/__init__.py
@@ -1,29 +1,127 @@
+from hmac import new
 from os import getenv
 from flask import jsonify
 import math
 import csv
 
-def get_products_csv(product_id,page,per_page):
-    FILEPATH = getenv('FILEPATH')
+FILEPATH = getenv('FILEPATH')
+
+def extract_formated_products():
     data = []
-    
+
     with open(FILEPATH,'r') as products_csv:
         csv_dict_reader = csv.DictReader(products_csv)
 
         for row in csv_dict_reader:
-            data.append(row)
+            formated_row = row.copy()
+            for key, value in formated_row.items():
+                if key=='id': formated_row.update({key: int(value)})
+                if key=='price': formated_row.update({key: float(value)})
 
-    if product_id:
-        product = [product for product in data if product['id']==product_id]
+            data.append(formated_row)
+
+    return data
+
+def get_products_csv(product_id,page,per_page):
+    data_csv = extract_formated_products()
+
+    if product_id:     
+        product = [product for product in data_csv if product['id']==product_id]
         return jsonify(*product)
 
-    if type(page)==str or type(per_page)==str:
+    if isinstance(page,str) or isinstance(per_page,str):
         return {'msg': f'Algum dado não está de acordo'}, 400
 
-    number_of_pages = math.ceil(len(data)/per_page)
-    pages = [data[index*per_page:(index+1)*per_page] for index in range(number_of_pages)]
+    number_of_pages = math.ceil(len(data_csv)/per_page)
+    pages = [data_csv[index*per_page:(index+1)*per_page] for index in range(number_of_pages)]
     
     if page > number_of_pages:
         return {'msg': f'Ultrapassou o número de páginas.', 'Nº da última página': number_of_pages}, 400
     
     return jsonify(pages[page-1]),200
+
+def create_product_csv(payload):
+    #check payload
+    error = ({'msg': f'Algum dado não está de acordo'}, 400)
+    if 'name' not in payload or 'price' not in payload: return error
+    for key in payload.keys():
+        if key not in ['name','price']:
+            return error
+    if type(payload['price'])!=float: return error
+
+    data_csv = extract_formated_products()
+    payload.update({'id': data_csv[-1]['id']+1})
+
+    with open(FILEPATH,'a') as products_csv:
+        fieldnames = ['id','name','price']
+        csv_dict_writer = csv.DictWriter(products_csv, fieldnames=fieldnames)
+        csv_dict_writer.writerow(payload)
+         
+    return payload, 201
+
+def update_product_csv(payload, product_id):
+    #check payload:
+    error = ({'msg': f'Algum dado não está de acordo'}, 400)
+    for key in payload.keys():
+        if key not in ['name','price']:
+            return error
+    if type(payload['price'])!=float: return error
+    
+    data_csv = extract_formated_products()
+    new_data_csv = []
+    
+    #Search the product:
+    not_found = ({'error': f'product id {product_id} not found'}, 404)
+    for product in data_csv:
+        if product['id']==product_id:
+            product.update(payload)
+            new_data_csv.append(product)
+            break
+        else:
+            new_data_csv.append(product)
+    else:
+        return not_found
+
+    #Reuse the previous loop if it breaks before finishing data_csv:
+    if len(data_csv) != len(new_data_csv):
+        for product in data_csv[len(new_data_csv):]:
+            new_data_csv.append(product)
+    
+    with open(FILEPATH,'w') as products_csv:
+        fieldnames = ['id','name','price']
+        csv_dict_writer = csv.DictWriter(products_csv, fieldnames=fieldnames)
+        csv_dict_writer.writeheader()
+        csv_dict_writer.writerows(new_data_csv)
+        
+    return payload
+
+def delete_product_csv(product_id):
+    data_csv = extract_formated_products()
+    new_data_csv = []
+    deleted_product = None
+
+    #Search the product:
+    not_found = ({'error': f'product id {product_id} not found'}, 404)
+    for product in data_csv:
+        if product['id']==product_id:
+            deleted_product = product
+            break
+        else:
+            new_data_csv.append(product)
+    else:
+        return not_found
+    
+    #Reuse the previous loop if it breaks before finishing data_csv:
+    if len(data_csv)-1 != len(new_data_csv):
+        for product in data_csv[len(new_data_csv)+1:]:
+            new_data_csv.append(product)
+    
+    with open(FILEPATH,'w') as products_csv:
+        fieldnames = ['id','name','price']
+        csv_dict_writer = csv.DictWriter(products_csv, fieldnames=fieldnames)
+        csv_dict_writer.writeheader()
+        csv_dict_writer.writerows(new_data_csv)
+
+    return deleted_product
+
+
